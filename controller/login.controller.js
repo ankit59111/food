04f242490455f373395913111eddbbe377d473c9
ankit59111 <ankitsingh5991111@gmail.com>
2018-09-12 22:24:13 +0530

    succesfuly created login controller to store data

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const database = require("../config/database")();
 const loginModel = require("../models/login.model")();
-exports.render = function (req, res) {
+exports.render = (req, res)=> {
     const {email,password,username} =  req.body;
-    let login = new loginModel({email:"dfsff",password:"sedewfs",username:"sdfsa"});
+
+    let login = new loginModel({email,password,username});
     login.save(function (error,success) {
         if(error)
             console.log(error);
+        console.log(req.body);
         res.send("saved suucesfully \n ")
 
     })
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
    const index = require('../controller/index.controller');
    const login = require('../controller/login.controller');
-    app.get('/',index.render);
-    app.post("/login",login.render);
+    app.route('/').get(index.render);
+    app.route("/login").post(login.render);
 
 }
